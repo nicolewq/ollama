af870e98c6b4e94fbb6f55b2b57966574440a235
Michael Yang <mxyng@pm.me>
2024-06-11 14:03:42 -0700

    add capabilities

--- server/routes.go
+++ server/routes.go
@@ -122,8 +122,8 @@ func (s *Server) GenerateHandler(c *gin.Context) {
 		return
 	}
 
-	if model.IsEmbedding() {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "embedding models do not support generate"})
+	if !model.Has(CapabilityCompletion) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "model does not support completion"})
 		return
 	}
 
@@ -1290,8 +1290,8 @@ func (s *Server) ChatHandler(c *gin.Context) {
 		return
 	}
 
-	if model.IsEmbedding() {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "embedding models do not support chat"})
+	if !model.Has(CapabilityCompletion) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "model does not support chat completion"})
 		return
 	}
 
